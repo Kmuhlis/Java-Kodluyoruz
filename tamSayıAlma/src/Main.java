a58001470eefaaf04eaa91a69896bd7e987c607b
Muhlis Kocaaslan <93223751+Kmuhlis@users.noreply.github.com>
2022-02-18 15:43:49 +0300

    Add files via upload

---
+++ tamSayıAlma/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Bir sayı girin: ");
+
+        a = input.nextInt();
+        System.out.println("Girilen sayı: " + a);
+    }
+}
