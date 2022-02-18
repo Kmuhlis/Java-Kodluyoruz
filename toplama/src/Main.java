13fc6408d169d7f7db3da0056ddcfb30defcea97
Muhlis Kocaaslan <93223751+Kmuhlis@users.noreply.github.com>
2022-02-18 15:44:30 +0300

    Add files via upload

---
+++ toplama/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 10, b = 25, sayilar, toplam;
+
+        toplam = a + b;
+        System.out.println("Toplanan Sayılar: " + a + " " + b);
+        System.out.println("Toplam: " + toplam);
+    }
+}
